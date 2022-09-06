0bc0c909990624f1af64a443b72aaaa8c0cbf237
mui-z <93278577+mh-idea@users.noreply.github.com>
2022-09-07 02:26:37 +0900

    fix: fix control value

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def sin_wave():
     for degree in range(0, 360):
         seed = math.sin(math.radians(degree))
 
-        pwm.duty_u16(int(abs(seed * 65535 * 0.8)))
+        pwm.duty_u16(int(abs(seed * 65535 * 0.95)))
         time.sleep_ms(10)
 
 
