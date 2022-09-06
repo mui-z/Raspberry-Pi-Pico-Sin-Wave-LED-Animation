a71211f39b2465f3f8813daf2f1fd1502df39951
mui-z <93278577+mh-idea@users.noreply.github.com>
2022-09-07 00:53:04 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import math
+
+import time
+from machine import PWM, Pin
+
+pwm = PWM(Pin(25))
+
+
+def bless():
+    for i in range(65535):
+        pwm.duty_u16(i)
+
+    for i in range(65535, 0, -1):
+        pwm.duty_u16(i)
+
+
+def sin_wave():
+    for degree in range(0, 90):
+        seed = math.sin(math.radians(degree))
+
+        pwm.duty_u16(int(seed * 65535 * 0.8))
+        time.sleep_ms(10)
+
+    for degree in range(90, 0, -1):
+        seed = math.sin(math.radians(degree))
+
+        pwm.duty_u16(int(seed * 65535 * 0.8))
+        time.sleep_ms(10)
+
+
+while True:
+    sin_wave()
